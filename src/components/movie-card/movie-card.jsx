9ebe362fff5a20999b18e72793e17e80a2f018d4
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-10 21:13:01 +0200

    3.4 update
    movie-card proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+// display movies rendered on MainView
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+    return (
+      <div onClick={() => onMovieClick(movieData)} className="movie-card">{movieData.Title}</div>
+    );
   }
 }
+
+// setting up default values for MovieCard properties
+// ensuring values are strings and required
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+  }).isRequired,
+
+  onMovieClick: PropTypes.func.isRequired
+};
