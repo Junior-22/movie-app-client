edc746947ef3665f157991e68209a9ff9f372b31
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-13 14:45:10 +0200

    Update navbar-view

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -6,7 +6,7 @@ import "./navbar-view.scss";
 export function NavbarView(user) {
 
   return (
-    <Navbar collapseOnSelect expand="lg" id="navbar" sticky="top">
+    <Navbar collapseOnSelect expand="lg" id="navbar" fixed="top">
       <Container id="navbar-container">
         <Navbar.Brand id="navbar-brand" href="logo">Movie-app</Navbar.Brand>
         <Nav id="nav" className="ms-auto">
