72d03c765e092f35e7be863b25ad659ddeb9bd8e
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-16 14:16:20 +0200

    Update

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import "./movie-card.scss";
 
+// display movies rendered on MainView
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+    return (
+      <Card className="movie-card" id="movie-card">
+        <Card.Img variant="top" src={movieData.ImagePath} crossorigin="true" />
+        <Card.Body>
+          <Card.Title id="movie-title">{movieData.Title}</Card.Title>
+          <Card.Text id="movie-description">{movieData.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movieData)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
+
+// setting up default values for MovieCard properties
+// ensuring values are strings and required
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+  }).isRequired,
+
+  onMovieClick: PropTypes.func.isRequired
+};
