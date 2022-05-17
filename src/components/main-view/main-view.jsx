d4048418891564360dabe73bdd098d42af8053c2
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-17 22:52:46 +0200

    Update main-view
    comment out registration for testing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -87,7 +87,7 @@ export class MainView extends React.Component {
     const { movies, selectedMovie, user, registered } = this.state;
 
     // registration for testing
-    if (!registered) return <RegistrationView onRegister={(registered) => this.onRegister(registered)} />;
+    //if (!registered) return <RegistrationView onRegister={(registered) => this.onRegister(registered)} />;
 
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
