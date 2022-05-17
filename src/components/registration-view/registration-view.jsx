7b28fa21b05bc36fc21bd886fa4338e6abf9305a
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-17 21:00:27 +0200

    Update registration-view
    added form validation

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,19 +1,71 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Form, FormControl, Button, Container, Row, Col, Card, CardGroup } from "react-bootstrap";
+import axios from "axios";
+import { Form, Button, Container, Row, Col, Card, CardGroup } from "react-bootstrap";
+import { Link } from "react-router-dom"
 import "./registration-view.scss";
 
 // registration form
 export function RegistrationView(props) {
-  const [username, setUsername] = useState(" ");
-  const [password, setPassword] = useState(" ");
-  const [email, setEmail] = useState(" ");
-  const [birthday, setBirthday] = useState(" ");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+  const [birthdayErr, setBirthdayErr] = useState("");
+
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username required");
+      isReq = false;
+    } else if (username.length < 5) {
+      setUsernameErr("Username must be 5 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr("Password must be 6 characters long");
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr("Email required");
+      isReq = false;
+    } else if (email.indexOf('@') === -1) {
+      setEmailErr("Email is invalid");
+      isReq = false;
+    }
+
+    return isReq;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    props.onRegister(false);
+    const isReq = validate();
+    if (isReq) {
+      axios.post("https://movies2022app.herokuapp.com/users/register", {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      })
+        .then(response => {
+          const data = response.data;
+          console.log(data);
+          alert("Registration successful, please login");
+          window.open("/", "_self"); // "_self" is necessary for the page to open in same tab
+        })
+        .catch(response => {
+          console.error(response);
+          alert("something not rightly entered");
+        });
+    }
   };
 
   return (
@@ -25,40 +77,47 @@ export function RegistrationView(props) {
               <Card.Body>
                 <Card.Title style={{ textAlign: "center", fontSize: 20 }}>Please register</Card.Title>
                 <Form>
-                  <Form.Group>
+                  <Form.Group controlId="formUsername">
                     <Form.Label>Username</Form.Label>
                     <Form.Control
                       type="text"
                       value={username}
-                      onChange={e => setUsername(e.target.value)}
-                      placeholder="Enter a username" />
+                      onChange={e => setUsername(e.target.value)} />
+                    {/* code to display validation error */}
+                    {usernameErr && <p>{usernameErr}</p>}
                   </Form.Group>
 
-                  <Form.Group>
+                  <Form.Group controlId="formPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                       type="password"
                       value={password}
                       onChange={e => setPassword(e.target.value)} />
+                    {/* code to display validation error */}
+                    {passwordErr && <p>{passwordErr}</p>}
                   </Form.Group>
 
-                  <Form.Group>
+                  <Form.Group controlId="Email">
                     <Form.Label>Email</Form.Label>
                     <Form.Control
                       type="email"
                       value={email}
                       onChange={e => setEmail(e.target.value)} />
+                    {/* code to display validation error */}
+                    {emailErr && <p>{emailErr}</p>}
                   </Form.Group>
 
-                  <Form.Group>
+                  <Form.Group controlId="updateBirthday">
                     <Form.Label>Birthday</Form.Label>
                     <Form.Control
-                      type="text"
+                      type="date"
                       value={birthday}
                       onChange={e => setBirthday(e.target.value)} />
                   </Form.Group>
 
-                  <Button variant="primary" type="submit">Submit</Button>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                  <P></P>
+                  <p>Already registered <Link to={"/"}>Sign in</Link>here</p>
 
                 </Form>
               </Card.Body>
@@ -69,3 +128,12 @@ export function RegistrationView(props) {
     </Container>
   );
 }
+
+RegistrationView.propTypes = {
+  register: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    Birthday: PropTypes.string.isRequired
+  }),
+};
