664e4790106768cac6db21607ea220166e5a768c
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-19 09:38:58 +0200

    Create director-view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom"
+
+import "./director-view.scss";
+
+export class DirectorView extends React.Component {
+
+  render() {
+    const { director, onBackClick } = this.props;
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card className="director-view" id="director-view">
+              <Card.Body>
+                <Card.Img className="movie-poster" id="movie-poster" variant="top" src={movieData.ImagePath} crossorigin="true" />
+                <Card.Title id="movie-title">{movieData.Title}</Card.Title>
+                <Card.Text className="movie-director" id="movie-director"> Director: {movieData.Director.Name}</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
+
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+  }).isRequired,
+
+  onBackClick: PropTypes.func.isRequired
+};
