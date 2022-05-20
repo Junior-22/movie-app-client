53bc0b04c042a3e11bce51fc78b002e7f4f7a288
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-20 12:03:35 +0200

    Update genre-view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Card, Col, Container, Row, Button } from "react-bootstrap";
-import { Link } from "react-router-dom"
 
 import "./genre-view.scss";
 
 export class GenreView extends React.Component {
 
   render() {
-    const { genre, onBackClick } = this.props;
+    const { movieData, onBackClick } = this.props;
 
     return (
       <Container>
@@ -16,13 +15,13 @@ export class GenreView extends React.Component {
           <Col>
             <Card className="genre-view" id="genre-view">
               <Card.Body>
-                <Card.Img className="movie-poster" id="movie-poster" variant="top" src={movieData.ImagePath} crossorigin="true" />
+                <Card.Img className="movie-poster" id="movie-poster" variant="top" src={movieData.ImagePath} crossOrigin="true" />
                 <Card.Title id="movie-title">{movieData.Title}</Card.Title>
                 <Card.Text className="movie-genre" id="movie-genre"> Genre: {movieData.Genre.Name}</Card.Text>
               </Card.Body>
             </Card>
 
-            <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
+            <Button id="" onClick={() => { onBackClick(null); }}>Back</Button>
 
           </Col>
         </Row>
