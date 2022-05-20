93e957a32891d22465f224ad2331f6a09263c65b
Junior-22 <juniorfrancis2608@gmail.com>
2022-05-20 20:44:22 +0200

    Update navbar-view
    Hide sign up if token exists

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,20 +1,47 @@
 import React from "react";
-import { Navbar, Container, Nav, Button, Form, FormControl, Offcanvas } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 import "./navbar-view.scss";
 
-export function NavbarView(user) {
+export function NavbarView({ user }) {
+
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  }
+
+  const isAuth = () => {
+    if (typeof window == "undefined") {
+      return false;
+    }
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
 
   return (
-    <Navbar collapseOnSelect expand="sm" id="navbar" fixed="top">
+    <Navbar collapseOnSelect expand="sm" id="navbar" sticky="top">
       <Container id="navbar-container">
         <Navbar.Brand id="navbar-brand" href="logo">Movie-app</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
         <Nav id="nav" className="ms-auto">
-          <Nav.Link id="nav-link" href="#home">Home</Nav.Link>
-          <Nav.Link id="nav-link" href="#features">Features</Nav.Link>
-          <Nav.Link id="nav-link" href="#register">Register</Nav.Link>
-          <Nav.Link id="nav-link" href="#logout">Logout</Nav.Link>
+          {isAuth() && (
+            <Nav.Link id="nav-link" href={`/users/${user}`}>{user}</Nav.Link>
+          )}
+          {isAuth() && (
+            <Button id="nav-link" variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
+          )}
+          {!isAuth() && (
+            <Nav.Link id="nav-link" href="/">Sign-in</Nav.Link>
+          )}
+          {!isAuth() && (
+            <Nav.Link id="nav-link" href="/register">Sign-up</Nav.Link>
+          )}
         </Nav>
+
       </Container>
     </Navbar>
   )
