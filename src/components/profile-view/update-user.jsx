7886f281886791e913dfcebca46c621ec166f569
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-01 17:54:40 +0200

    fix code

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -3,47 +3,51 @@ import { Row, Col, Button, Card, Form } from "react-bootstrap";
 import "./update-user.scss"
 
 export function UpdatedUser(props) {
+  console.log("UpdatedUser: ", props)
 
-  const { handleSubmit, handleUpdate, userData } = props;
+  const { handleSubmit, handleUpdate, updatedUser } = props;
 
   return (
     <Row>
       <Col>
         <Card id="update-user">
-          <Card.Title id="title">Update Profile</Card.Title>
-          <Form>
+          <Card.Body>
+            <Card.Title id="title">Update Profile</Card.Title>
+            <Form>
 
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                name="Username"
-                //defaultValue={userData.Username}
-                onChange={e => handleUpdate(e)} />
-            </Form.Group>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="Username"
+                  defaultValue={updatedUser.Username}
+                  onChange={e => handleUpdate(e)} />
+              </Form.Group>
 
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="text"
-                name="Password"
-                placeholder="new password required"
-                onChange={e => handleUpdate(e)} />
-            </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="Password"
+                  placeholder="new password required"
+                  onChange={e => handleUpdate(e)} />
+              </Form.Group>
 
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="text"
-                name="Email"
-                //defaultValue={userData.Email}
-                onChange={e => handleUpdate(e)} />
-            </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="Email"
+                  defaultValue={updatedUser.Email}
+                  onChange={e => handleUpdate(e)} />
+              </Form.Group>
 
-            <Button id="update-user-button" variant="primary" type="submit" onClick={handleSubmit}>Update</Button>
+              <Button id="update-user-button" variant="primary" type="submit" onClick={handleSubmit}>Update</Button>
 
-          </Form>
+            </Form>
+          </Card.Body>
         </Card>
+
       </Col>
     </Row>
   );
