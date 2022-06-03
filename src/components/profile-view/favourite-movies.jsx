e7c31e8ee19f5f784f13723ca6930d83811cd37b
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-03 14:43:23 +0200

    fix code

--- src/components/profile-view/favourite-movies.jsx
+++ src/components/profile-view/favourite-movies.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./favourite-movies.scss"
-import "../movie-view/movie-view";
 
 
 export function FavouriteMovies({ favouriteMoviesList, removeFav }) {
@@ -11,30 +10,32 @@ export function FavouriteMovies({ favouriteMoviesList, removeFav }) {
   return (
     <Container className="col-remove-padding">
       <Row>
-        <Col>
+        <Col xs={12}>
           <h3 id="title">Favourite Movies</h3>
-
-          {favouriteMoviesList.map((movieData) => {
-            return (
-              <Col key={movieData._id}>
-                <Card id="movie-view">
-                  <Card.Img variant="top" src={movieData.ImagePath} crossOrigin="true" />
-                  <Card.Body>
-                    <Card.Title>{movieData.Title}</Card.Title>
-                    <Card.Subtitle>{movieData.Year}</Card.Subtitle>
-                    <Link to={`/movies/${movieData._id}`}>
-                      <Button variant="link">Details</Button>
-                    </Link>
-
-                    <Button variant="primary" onClick={() => removeFav(movieData._id)}>Remove from favourites</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          })}
         </Col>
-
       </Row>
+      <Row>
+        {favouriteMoviesList.map((movieData) => {
+          return (
+            <Col xs={12} md={6} lg={3} key={movieData._id}>
+              <Card id="fav-movies">
+                <Card.Img variant="top" src={movieData.ImagePath} crossOrigin="true" />
+                <Card.Body>
+                  <Card.Title>{movieData.Title}</Card.Title>
+                  <Card.Subtitle>{movieData.Year}</Card.Subtitle>
+                  <Link to={`/movies/${movieData._id}`}>
+                    <Button variant="link">Details</Button>
+                  </Link>
+
+                  <Button variant="primary" onClick={() => removeFav(movieData._id)}>Remove from favourites</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+
+
     </Container>
   )
 }
