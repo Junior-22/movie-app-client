acbb9509bf962dc94d5516febfd792e2f54048c0
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-10 10:48:30 +0200

    Update

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom"
+import "./movie-card.scss";
 
+// display movies rendered on MainView
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
+    const { movieData, addMovieToFavourites, favourites } = this.props;
+    // console.log(favourites)
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+    const hideAddToFavouritesButton = favourites.find(id => id === movieData._id)
+
+    return (
+      <Card className="movie-card" id="movie-card">
+        <Card.Img variant="top" src={movieData.ImagePath} crossOrigin="true" />
+        <Card.Body>
+          <Card.Title className="movie-title" id="movie-title">{movieData.Title}</Card.Title>
+          <Card.Text className="year" id="year">{movieData.Year}</Card.Text>
+          <Link to={`/movies/${movieData._id}`}>
+            <Button variant="link">see more</Button>
+          </Link>
+          {hideAddToFavouritesButton ? "In favourites list" : <Button variant="dark" onClick={() => addMovieToFavourites(movieData._id)}>Add to Favourites</Button>}
+        </Card.Body>
+      </Card>
+    );
   }
 }
+
+// setting up default values for MovieCard properties
+// ensuring values are strings and required
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+  }).isRequired,
+
+  //onMovieClick: PropTypes.func.isRequired
+};
