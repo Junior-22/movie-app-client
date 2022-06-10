f85232e6bc9915c22627761f6ab05277183892d5
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-10 19:39:03 +0200

    create store for redux, wrap main view in a provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Container from "react-bootstrap/Container";
-import { MainView } from "./components/main-view/main-view";
+import { Container } from "react-bootstrap";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import movieApp from "./reducers/reducers";
+import MainView from "./components/main-view/main-view";
 
 // import statement indicating need to bundle './index.scss'
 import "./index.scss";
+
+const movieAppStore = createStore(movieApp, devToolsEnhancer());
 
 // Main component (will eventually use others)
 class MovieApplication extends React.Component {
   render() {
     return (
-      <Container className="movie-app">
-        <MainView />
-      </Container>
+      <Provider store={movieAppStore}>
+        <Container className="movie-app">
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
