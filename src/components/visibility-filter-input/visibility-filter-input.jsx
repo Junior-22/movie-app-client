7d72b38851641c2c00b77d4fdbffb5651e15f621
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-13 22:05:08 +0200

    add ID selector

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form } from "react-bootstrap";
 import { setFilter } from "../../actions/actions";
+import "./visibility-filter-input.scss";
 
 function VisibilityFilterInput(props) {
-  return <Form.Control
+  return <Form.Control id="search"
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
     placeholder="filter"
